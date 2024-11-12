249f3dfa7d687a970b4ed5288d9d6bb74777ed50
AishwaryaRajput28 <am.rajput2894@gmail.com>
2024-11-12 11:59:34 +0530

    ese

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
+// Set the base URL for Axios
+axios.defaults.baseURL = "http://localhost:4000";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
